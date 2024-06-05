7564b51ca5227db3e863a6c9ccdf90106541d04d
braidy08 <braidy08@duck.com>
2024-06-05 16:11:43 +1000

    lambo

--- hooks/preplanningtweakdata.lua
+++ hooks/preplanningtweakdata.lua
@@ -28,6 +28,43 @@ Hooks:PostHook(PrePlanningTweakData,"init","init_nrftwcausefunny_lev",function(s
 		cost = 0,
 		budget_cost = 0
 	}
+	self.types.nrftw_morethermite = {
+		name_id = "menu_nrftw_morethermite_name",
+		desc_id = "menu_nrftw_morethermite_desc",
+		category = "mission_equipment",
+		post_event = "preplan_02",
+		prio = 1,
+		icon = 51,
+		total = 1,
+		cost = 1000,
+		budget_cost = 1
+	}
+	self.types.nrftw_thermiteentrance = {
+		name_id = "menu_nrftw_thermiteentrance_name",
+		desc_id = "menu_nrftw_thermiteentrance_desc",
+		plan = "vault_plan",
+		pos_not_important = false,
+		category = "vault_plan",
+		post_event = "preplan_02",
+		prio = 1,
+		icon = 51,
+		total = 1,
+		cost = 0,
+		budget_cost = 0
+	}
+	self.types.nrftw_vaultlockdownsabotage = {
+		name_id = "menu_nrftw_vaultlockdownsabotage_name",
+		desc_id = "menu_nrftw_vaultlockdownsabotage_desc",
+		plan = "vault_plan",
+		pos_not_important = false,
+		category = "vault_plan",
+		post_event = "preplan_02",
+		prio = 1,
+		icon = 42,
+		total = 1,
+		cost = 5000,
+		budget_cost = 6
+	}
 	
 	
 end)
@@ -51,7 +88,8 @@ Hooks:PostHook(PrePlanningTweakData,"_create_locations","_create_locations_nrftw
 		mission_briefing_texture = "textures/preplanningtemplmao",
 		total_budget = 10,
 		default_plans = {
-				escape_plan = "nrftw_vannormal"
+				escape_plan = "nrftw_vannormal",
+				vault_plan = "nrftw_thermiteentrance"
 		},
 		start_location = {
 			group = "a",
