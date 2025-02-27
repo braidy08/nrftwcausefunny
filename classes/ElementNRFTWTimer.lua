242bc1c10b34a4a5d9e7be2f8edd22c29716110e
braidy08 <braidy08@duck.com>
2025-02-27 20:27:02 +1100

    bestupdate

---
+++ classes/ElementNRFTWTimer.lua
@@ -0,0 +1,6 @@
+-- yap yap yappity yap yap eventually
+--[[
+going to make this cause the normal timer element doesnt do things i want like
+- changing timer speed (the thermite in pd3 does this to my knowledge)
+- thats about it
+]]
