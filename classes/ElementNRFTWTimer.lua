9eb03fd2f1c4992ae60db654bc70ee916087112d
braidy08 <braidy08@duck.com>
2025-03-02 01:41:36 +1100

    removethatonecommentin ElementNRFTWTimer

--- classes/ElementNRFTWTimer.lua
+++ classes/ElementNRFTWTimer.lua
@@ -1,4 +1,3 @@
--- yap yap yappity yap yap eventually
 --[[
 going to make this cause the normal timer element doesnt do things i want like
 - changing timer speed (the thermite in pd3 does this to my knowledge)
